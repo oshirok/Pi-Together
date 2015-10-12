6d6a6ea8647f37e0e9328c108e3caecd784ecd49
Kent O <noodledevelopmentco@gmail.com>
2015-10-12 11:58:23 +0900

    Added a digit verification step

--- routes/index.js
+++ routes/index.js
@@ -30,13 +30,23 @@ function piScript(lo, hi, precis) {
 	"});";
 }
 
+function verify(calculatedPi) {
+	var accuratePi = "3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643086021394946395224737190702179860943702770539217176293176752384674818467669405132000568127145263560827785771342757789609173637178721468440901224953430146549585371050792279689258923542019956112129021960864034418159813629774771309960518707211349999998372978049951059731732816096318595024459455346908302642522308253344685035261931188171010003137838752886587533208381420617177669147303598253490428755468731159562863882353787593751957781857780532171226806613001927876611195909216420198";
+	var correct = 0;
+	while(correct < 1000 && calculatedPi.charAt(correct) == accuratePi.charAt(correct)) {
+		correct++;
+	}
+	return correct;
+}
+
 var num = 0;
-var max = 1000;
-var increment = 25;
+var max = 333; // number of iterations
+var precision = 1000; // number of places to calculate for
+var increment = 1;
 var startTime = null;
 var endTime = null;
 var pi = new Decimal(0);
-Decimal.config({ precision: max, rounding: 4 });
+Decimal.config({ precision: precision, rounding: 4 });
 var hi = 0;
 var jobs = {};
 
@@ -54,7 +64,7 @@ router.get('/work', function(req, res, next) {
 	if (num < max) {
 		var progress = Math.round(100 * num / max);
     	res.setHeader('Content-Type', 'application/json');
-    	var job = JSON.stringify({isComplete: false, id: num, progress: progress, params: {lo: num, hi: num + increment, precis: max}});
+    	var job = JSON.stringify({isComplete: false, id: num, progress: progress, params: {lo: num, hi: num + increment, precis: precision}});
     	res.send(job);
 		jobs[num+""] = job;
 		num += increment;
@@ -66,7 +76,7 @@ router.get('/work', function(req, res, next) {
 	} else {
 		if (!endTime) {
 			endTime = new Date();
-			console.log("RESULT: " + pi.toFixed(max));
+			console.log("RESULT: " + pi.toFixed(precision));
 		}
 		res.setHeader('Content-Type', 'application/json');
     	res.send(JSON.stringify({isComplete: true}));
@@ -75,13 +85,14 @@ router.get('/work', function(req, res, next) {
 
 router.post('/work', function(req, res, next) {
 	var jobId = req.body.id;
-	console.log("DELETING JOB ID: " + jobId);
+	// console.log("DELETING JOB ID: " + jobId);
 	if(jobs[jobId+""]) {
 		delete jobs[jobId+""];
-		console.log(Object.keys(jobs));
+		// console.log(Object.keys(jobs));
 		var result = new Decimal(req.body.data);
-		console.log("RECIEVED: " + result);
+		// console.log("RECIEVED: " + result);
 		pi = pi.plus(result);
+		console.log("Correct digits: " + verify(""+pi));
 		res.send(200);
 	}
 });
