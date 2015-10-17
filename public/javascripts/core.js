b15da5221f36e19a80563f05729c7231f24da07e
Kent <noodledevelopmentco@gmail.com>
2015-10-17 23:14:58 +0900

    added comments

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -3,14 +3,14 @@ var piTogether = angular.module('piTogether', ['ui.bootstrap']);
 
 piTogether.controller("mainController",function($scope,$http,$location){
 
+  // Progress variable
   $scope.progress = 0;
+  // thr result
   $scope.result = null;
 
-
-
+  // gets the number of thrreads specified in url parameter
   var numberOfCores = $location.search().threads || navigator.hardwareConcurrency || 2;
-  console.log("number of cores determined: " + numberOfCores);
-  console.log($location.search());
+  console.log("WORKING WITH " + numberOfCores + " CORES");
   var workers = [];
   for(var i = 0; i < numberOfCores; i++) {
     var worker = new Worker('javascripts/worker.js');
